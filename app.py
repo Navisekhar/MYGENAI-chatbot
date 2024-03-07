077638d9699486063745cda897b9b33f84f1906e
tvmohanrao <138654263+tvmohanrao@users.noreply.github.com>
2024-03-07 12:26:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain import LLMChain, PromptTemplate
 from langchain.memory import ConversationBufferMemory
 
-OPENAI_API="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"
-os.environ["OPENAI_API"] = OPENAI_API
+OPENAI_API_KEY="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"
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 template = """You are a helpful assistant to answer all user queries.
 {chat_history}
